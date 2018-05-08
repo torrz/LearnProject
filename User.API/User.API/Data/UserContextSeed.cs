714db13b15fb228cdc11ee064e7a5bb5101c678c
温振刚 <opnicorobin@hotmail.com>
2018-05-08 22:12:17 +0800

    新项目

---
+++ User.API/User.API/Data/UserContextSeed.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace User.API.Data
+{
+    public class UserContextSeed
+    {
+    }
+}
---
+++ User.API/User.API/Models/BpFile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace User.API.Models
+{
+    public class BPFile
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// 文件名称
+        /// </summary>
+        public string FileName { get; set; }
+
+        /// <summary>
+        /// 上传源文件的地址
+        /// </summary>
+        public string OriginFilePath { get; set; }
+
+        /// <summary>
+        /// 格式转化后的文件地址
+        /// </summary>
+        public string FromatFilePath { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreatedTime { get; set; }
+
+    }
+}
