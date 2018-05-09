79b61d9771b4bedcdf5bd85e2d837485b75fae50
温振刚 <opnicorobin@hotmail.com>
2018-05-09 21:59:13 +0800

    修改get方法返回mysql的数据

--- User.API/User.API/Controllers/ValuesController.cs
+++ User.API/User.API/Controllers/ValuesController.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using User.API.Data;
 
 namespace User.API.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        private UserContext _userContext;
+
+        public ValuesController(UserContext userContext)
+        {
+            _userContext = userContext;
+        }
+
         // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<IActionResult> Get()
         {
-            return new string[] { "value1", "value2" };
+            return Json(await _userContext.Users.SingleOrDefaultAsync(u => u.Name == "torrz"));
         }
 
         // GET api/values/5
